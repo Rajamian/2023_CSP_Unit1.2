36f4195a02e3194dee9ded659c68aee787f82f71
Ryan <ryan.ajamian@vtsd.com>
2023-11-28 08:15:02 -0500

    a

--- 1.2.2/leaderboard.py
+++ 1.2.2/leaderboard.py
@@ -19,13 +19,16 @@ def get_names(file_name):
         index = 0
 
         # TODO 1: use a while loop to read the leader name from the line (format is "leader_name,leader_score")
+        while (line[index] != ","):
+            leader_name = leader_name +line[index]
+            index = index + 1
 
         # TODO 2: add the player name to the names list
-
+        names.append(leader_name)
     leaderboard_file.close()
 
     #  TODO 6: return the names list in place of the empty list
-    return []
+    return names
 
 
 # return scores from the leaderboard file
@@ -38,15 +41,20 @@ def get_scores(file_name):
         index = 0
 
         # TODO 3: use a while loop to index beyond the comma, skipping the player's name
+        while(line[index] != ","):
+            index += 1
+        index += 1
 
         # TODO 4: use a while loop to get the score
-
+        while line[index] != "\n":
+            leader_score += line[index]
+            index += 1
         # TODO 5: add the player score to the scores list
-
+        scores.append(leader_score)
     leaderboard_file.close()
 
     # TODO 7: return the scores in place of the empty list
-    return []
+    return scores
 
 
 # update leaderboard by inserting the current player and score to the list at the correct position
