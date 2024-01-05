354b16891a64ad15d1a3ec7cd14fb77ab537375d
Ryan <ryan.ajamian@vtsd.com>
2024-01-05 08:17:45 -0500

    a

--- Main/Game.py
+++ Main/Game.py
@@ -1,2 +1,37 @@
 import turtle as trtl
 
+line = trtl.Turtle()
+
+
+#make game board
+line.speed(0)
+
+line.penup()
+line.goto(-125,-300)
+line.pendown()
+line.left(90)
+line.forward(600)
+line.penup()
+line.goto(125,-300)
+line.pendown()
+line.forward(600)
+line.penup()
+line.goto(-300,125)
+line.pendown()
+line.right(90)
+line.forward(600)
+line.penup()
+line.goto(300,-125)
+line.pendown()
+line.right(180)
+line.forward(600)
+
+#make x
+
+
+
+
+
+
+wn = trtl.Screen()
+wn.mainloop()
