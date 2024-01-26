bb889ba02a8746090faaa515988bfb51add8315f
Ryan <ryan.ajamian@vtsd.com>
2024-01-26 07:43:03 -0500

    a

---
+++ Exampletemp/temp.py
@@ -0,0 +1,23 @@
+# Write a method called addSubtract()
+# This method should take a parameter called operation
+# When the method is called, ask the user for two numbers
+# Do the operation of the parameter to the inputs
+
+num1 = [2, 5, 6, 98, 32, 123]
+num2 = [5, 67, 89, 34, 1, 4]
+
+def addSubtract(operation):
+    num1 = [2, 5, 6, 98, 32, 123]
+    num2 = [5, 67, 89, 34, 1, 4]
+    for adding in range(1):
+        total = sum(num1)
+        print(total)
+        total2 = sum(num2)
+        print(total2)
+
+
+
+
+
+addSubtract("add")
+addSubtract("subtract")
